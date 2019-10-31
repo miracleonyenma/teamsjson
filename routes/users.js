5e63c6bc288d82624ed572eef3da8dc34d4dbe75
miracleonyenma <miracleficient@gmail.com>
2019-10-31 14:02:14 +0100

    added get route

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,48 @@ const userRoutes = (app, fs) => {
   // variables
   const dataPath = './data/team.json';
 
-  //read
+
+
+  // //read
+  // app.get('/users', (req, res) => {
+  //   fs.readFile(dataPath, 'utf8', (err, data) => {
+  //     if (err){
+  //       console.log(err);
+  //     }
+  //     res.send(JSON.parse(data));
+  //   });
+  // });
+
+// refactored helper methods
+
+  const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
+    fs.readFile(filePath, encoding, (err, data) => {
+      if(err){
+        throw err;
+      };
+
+      callback(returnJson ? JSON.parse(data) : data);
+    });
+  };
+
+  const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+
+    fs.writeFile(filePath, fileData, encoding, (err, data) => {
+      if(err){
+        throw err;
+      }
+
+      callback();
+    });
+  };
+
+  // read
   app.get('/users', (req, res) => {
     fs.readFile(dataPath, 'utf8', (err, data) => {
-      if (err){
-        console.log(err);
+      if(err){
+        throw err;
       }
+
       res.send(JSON.parse(data));
     });
   });
