8e6bc91dd94c25a5ca4afb1a95b7d89e25adc472
miracleonyenma <miracleficient@gmail.com>
2019-11-04 13:52:37 +0100

    post request finally works

--- routes/users.js
+++ routes/users.js
@@ -38,6 +38,25 @@ const userRoutes = (app, fs) => {
     });
   };
 
+  app.post('/users', (req, res) => {
+
+        readFile(data => {
+            const newUserId = data.members.length;
+
+            console.log(newUserId);
+            // add the new user
+            data.members[newUserId.toString()] = req.body;
+            console.log(req.body);
+
+            writeFile(JSON.stringify(data, null, 2), () => {
+                res.status(200).send("new user added");
+            });
+            console.log(data.members[5]);
+
+        },
+            true);
+    });
+
   // read
   app.get('/users', (req, res) => {
     fs.readFile(dataPath, 'utf8', (err, data) => {
